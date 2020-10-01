26cd5ddb9fa0cb33ab9236fc2660514be8b71309
drdrenckhahn <darlrdren22@gmail.com>
2020-09-30 20:40:52 -0600

    Update after getting project to run

---
+++ RuntimeTerrors/desktop/src/com/csc14/runtimeterrors/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csc14.runtimeterrors.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csc14.runtimeterrors.game.OmegaChess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OmegaChess(), config);
+	}
+}
