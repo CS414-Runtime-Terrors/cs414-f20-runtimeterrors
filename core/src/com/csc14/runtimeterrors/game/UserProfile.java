7cdc4d513c29d9fea3e8fb668e63c09043f1eade
dmccoystephenson <dmccoystephenson@gmail.com>
2020-10-01 23:06:11 -0600

    added fields and a constructor

--- core/src/com/csc14/runtimeterrors/game/UserProfile.java
+++ core/src/com/csc14/runtimeterrors/game/UserProfile.java
@@ -2,6 +2,22 @@ package com.csc14.runtimeterrors.game;
 
 public class UserProfile {
 
+    private String nickname = null;
+    private String password = null;
+    private String emailAddress = null;
+    private int gamesWon = -1;
+    private int gamesLost = -1;
+    private int gamesTied = -1;
+
+    public UserProfile(String nick, String pass, String email) {
+        nickname = nick;
+        password = pass;
+        emailAddress = email;
+        gamesWon = 0;
+        gamesLost = 0;
+        gamesTied = 0;
+    }
+
 
 
 }
