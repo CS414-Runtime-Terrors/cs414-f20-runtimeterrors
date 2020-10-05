60f379b0a4de1781778467cc2164c678a0bd4358
artemisstreet <falkyn.worm@gmail.com>
2020-10-05 16:57:10 -0600

    implemented Champion legalMoves

--- server/src/main/java/com/omegaChess/pieces/Champion.java
+++ server/src/main/java/com/omegaChess/pieces/Champion.java
@@ -10,4 +10,28 @@ public class Champion extends ChessPiece{
             return "\u2617";
     }
 
+    @Override
+    public ArrayList<String> legalMoves(){
+        ArrayList<String> moves = new ArrayList<>();
+        String pos = this.getPosition();
+        int[] rc = new int[2];
+        try{
+            rc = parsePosition(pos);
+        }catch (IllegalPositionException e){
+            System.out.println("Exception thrown: " + e);
+        }
+        for (int i = 1; i <= 2; i++){
+            moves.add(reverseParse(rc[0]+i, rc[1]));
+        } for (int i = 1; i <= 2; i++){
+            moves.add(reverseParse(rc[0]-i, rc[1]));
+        } for (int i = 1; i <= 2; i++){
+            moves.add(reverseParse(rc[0], rc[1]+i));
+        } for (int i = 1; i <= 2; i++){
+            moves.add(reverseParse(rc[0], rc[1]-i));
+        }
+        moves.add(reverseParse(rc[0]+2, rc[1]-2)); moves.add(reverseParse(rc[0]+2, rc[1]+2));
+        moves.add(reverseParse(rc[0]-2, rc[1]+2)); moves.add(reverseParse(rc[0]-2, rc[1]-2));
+        return moves;
+    }
+
 }
