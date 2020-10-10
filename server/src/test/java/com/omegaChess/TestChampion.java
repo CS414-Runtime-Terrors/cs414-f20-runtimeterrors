811b114188acae42b955670f2d1551ea313264d5
artemisstreet <falkyn.worm@gmail.com>
2020-10-09 18:06:00 -0600

    Updated my test classes to better represent those of test methods for JUnit 5

--- server/src/test/java/com/omegaChess/TestChampion.java
+++ server/src/test/java/com/omegaChess/TestChampion.java
@@ -7,6 +7,7 @@ import com.omegaChess.pieces.Champion;
 import com.omegaChess.pieces.ChessPiece;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("JUnit Champion Class Test")
 class TestChampion {
@@ -15,7 +16,7 @@ class TestChampion {
     public void testGetColor(){
         ChessBoard board = new ChessBoard();
         Champion champ = new Champion(board, ChessPiece.Color.WHITE);
-        assert champ.getColor() == ChessPiece.Color.WHITE: "Expected color to be white";
+        assertEquals(ChessPiece.Color.WHITE, champ.getColor(), "Expected color to be white");
     }
 
     @Test
@@ -23,7 +24,7 @@ class TestChampion {
         ChessBoard board = new ChessBoard();
         Champion champ = new Champion(board, ChessPiece.Color.WHITE);
         board.placePiece(champ, "a4");
-        assert champ.getPosition().equals("a4"): "Piece expected to return \"a4\"";
+        assertEquals("a4", champ.getPosition(), "Piece expected to return \"a4\"");
     }
 
     @Test
@@ -35,6 +36,6 @@ class TestChampion {
         moves.add("c8"); moves.add("c9"); moves.add("c6"); moves.add("c5");
         moves.add("d7"); moves.add("e7"); moves.add("b7"); moves.add("a7");
         moves.add("a9"); moves.add("e9"); moves.add("e5"); moves.add("a5");
-        assert moves.equals(champ.legalMoves()): "Expected moves not provided";
+        assertEquals(champ.legalMoves(), moves, "Expected moves not provided");
     }
 }
