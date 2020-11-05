3548369441659035ff59e4affad60d2732fe38d5
Patrick Lee <leepat@rams.colostate.edu>
2020-11-04 17:44:02 -0700

    Added listeners to all BoardSquare objects

--- core/src/com/csc14/runtimeterrors/game/BoardAssets/BoardSquare.java
+++ core/src/com/csc14/runtimeterrors/game/BoardAssets/BoardSquare.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class BoardSquare extends Actor {
 
     public Sprite currentPiece = null;
-    public Color squareColor;
+    public final Color squareColor;
 
     public BoardSquare(Color squareColor) {
         this.setColor(squareColor);
@@ -22,19 +22,38 @@ public class BoardSquare extends Actor {
 
     public BoardSquare(Color squareColor, String piece) {
         this.setColor(squareColor);
+        this.squareColor = squareColor;
         this.setHeight(30);
         this.setWidth(30);
+
+        //set piece depending on the file given
         this.currentPiece = new Sprite(new Texture(Gdx.files.internal(piece)));
+
+        //set size to 30x30
         currentPiece.setSize(30, 30);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
+        float x = this.getX();
+        float y = this.getY();
+        float width = this.getWidth();
+        float height = this.getHeight();
+
         //draw board square
         ShapeRenderer shapeRenderer = new ShapeRenderer();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(this.getColor());
         shapeRenderer.rect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+        shapeRenderer.end();
+
+        //outline square in black
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.line(x, y, x, y + height);
+        shapeRenderer.line(x, y + height, x + width, y + height);
+        shapeRenderer.line(x + width, y +height, x + width, y);
+        shapeRenderer.line(x + width, y, x, y);
         shapeRenderer.end();
 
         //draw piece if there is a piece to draw
@@ -48,18 +67,20 @@ public class BoardSquare extends Actor {
         }
     }
 
-    public void setPiece(String piece) {
-        this.currentPiece = new Sprite(new Texture(Gdx.files.internal(piece)));
-    }
+    public void setPiece(Sprite piece) { this.currentPiece = piece; }
 
     public void removePiece() {
         this.currentPiece = null;
     }
 
+    public boolean hasPiece() { return !(this.currentPiece == null); }
+
+    public boolean isHighlighted() { return this.getColor().equals(Color.YELLOW); }
+
     public void highlight() {
         this.setColor(Color.YELLOW);
     }
 
-    public void unHighlight() {this.setColor(this.squareColor);}
+    public void unHighlight() { this.setColor(squareColor); }
 
 }
