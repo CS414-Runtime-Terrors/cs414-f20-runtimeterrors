86737c7cb99fb4232d000f480ce0aff1f8e880b5
dmccoystephenson <dmccoystephenson@gmail.com>
2020-11-06 19:17:04 -0700

    added TestOCServerData.java to test saving/loading

---
+++ server/src/test/java/com/omegaChess/TestOCServerData.java
@@ -0,0 +1,35 @@
+package com.omegaChess;
+
+import com.omegaChess.server.OCServerData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("JUnit OCServerData Class Test")
+public class TestOCServerData {
+
+    @Test
+    public void testSavingAndLoading() {
+        // create data object to save
+        OCServerData dataToSave = new OCServerData("./test-data/");
+
+        // add data
+        dataToSave.createProfile("Daniel", "pass", "daniel@gmail.com");
+
+        // save data
+        dataToSave.save();
+
+        // create data object to load
+        OCServerData loadedData = new OCServerData("./test-data/");
+        loadedData.load();
+
+        // make assertions
+        assertTrue(loadedData.profileExists("Daniel"));
+
+        // delete data
+        loadedData.deleteRootSaveFolder();
+
+    }
+
+}
