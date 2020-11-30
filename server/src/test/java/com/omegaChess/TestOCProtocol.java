4ac2e54fa86dc4502b91e62e144788043d1303b6
Falkyn <34254926+ArteyStreet@users.noreply.github.com>
2020-11-30 15:16:16 -0700

    Update TestOCProtocol.java
    Removed unnecessary import

--- server/src/test/java/com/omegaChess/TestOCProtocol.java
+++ server/src/test/java/com/omegaChess/TestOCProtocol.java
@@ -1,7 +1,6 @@
 package com.omegaChess;
 
 import com.omegaChess.server.*;
-import javafx.beans.binding.IntegerBinding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
